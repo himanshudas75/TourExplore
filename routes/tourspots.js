13f15355efaed141f972391f01b4bcffc1775a14
Himanshu Das <himanshudas75@gmail.com>
2023-12-09 12:53:13 +0530

    Added login functionality

--- routes/tourspots.js
+++ routes/tourspots.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
+const { isLoggedIn } = require("../middleware");
 
 const Tourspot = require("../models/tourspot");
 
@@ -24,7 +25,7 @@ router.get("/", catchAsync(async (req, res) => {
     res.render("tourspots/index", { tourspots });
 }));
 
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("tourspots/new");
 });
 
@@ -37,7 +38,7 @@ router.get("/:id", catchAsync(async (req, res) => {
     res.render("tourspots/show", { tourspot });
 }));
 
-router.get("/:id/edit", catchAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, catchAsync(async (req, res) => {
     const tourspot = await Tourspot.findById(req.params.id);
     if (!tourspot) {
         req.flash('error', 'Cannot find that Tourist Spot!');
@@ -46,21 +47,21 @@ router.get("/:id/edit", catchAsync(async (req, res) => {
     res.render("tourspots/edit", { tourspot });
 }));
 
-router.post("/", validateTourspots, catchAsync(async (req, res) => {
+router.post("/", isLoggedIn, validateTourspots, catchAsync(async (req, res) => {
     const tourspot = new Tourspot(req.body.tourspot);
     await tourspot.save();
     req.flash('success', 'Successfully created new Tourist Spot!');
     res.redirect(`/tourspots/${tourspot._id}`);
 }));
 
-router.put("/:id", validateTourspots, catchAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, validateTourspots, catchAsync(async (req, res) => {
     const { id } = req.params;
     const tourspot = await Tourspot.findByIdAndUpdate(id, { ...req.body.tourspot });
     req.flash('success', 'Successfully updated Tourist Spot!');
     res.redirect(`/tourspots/${tourspot._id}`);
 }));
 
-router.delete("/:id", catchAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Tourspot.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted Tourist Spot!');
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 
 const catchAsync = require("../utils/catchAsync");
 
 const User = require("../models/user");
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -22,5 +24,14 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('/register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/tourspots');
+});
 
 module.exports = router;
